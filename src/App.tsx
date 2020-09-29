4211ce7ce78c97fa402309e47c40ab518e6c05cd
Aaron Sykes <aaron.sykes@airship.com>
2020-09-28 20:30:23 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        Minimal React Template
+      </header>
+    </div>
+  );
+}
+
+export default App
